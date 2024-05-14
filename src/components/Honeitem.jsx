cacd28ef3c36af4e43cdd687b4b4153407386c98
rohit <rohitindurke@gmail.com>
2024-05-14 18:04:02 +0530

    add files

---
+++ src/components/Honeitem.jsx
@@ -0,0 +1,24 @@
+import item1 from './imgs/item1.png'
+
+
+const HomeItem = ({item}) =>{
+    return(
+        <>
+           <div className="item-container">
+      <img className="item-image" src={item1} alt="item image"/>
+      <div className="rating">
+            {item.rating.stars} ⭐ | ${item.rating.count}
+      </div>
+      <div className="company-name">{item.company}</div>
+      <div className="item-name">{item.item_name}</div>
+      <div className="price">
+          <span className="current-price">Rs {item.current_price}</span>
+          <span className="original-price">Rs {item.original_price}</span>
+          <span className="discount">(${item.discount_percentage}% OFF)</span>
+      </div>
+      <button className="btn-add-bag" onclick={()=>{console.log("item hass been added")}}>Add to Bag</button>
+    </div>
+        </>
+    )
+}
+export default HomeItem;

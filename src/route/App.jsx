843831c72cfc06545a7aad33d362e4865b0ff78e
rohit <rohitindurke@gmail.com>
2024-05-15 11:53:26 +0530

    mintra-clone-react

--- src/route/App.jsx
+++ src/route/App.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import Header from "../components/Header";
 import Footer from '../components/Footer'
 import Home from "./Home";
+import Bag from './Bag'
 
 
 let App = () =>{
     return(
         <>
             <Header/>
-            
+            <Bag/>
             <Home/>
             <Footer/>
         </>

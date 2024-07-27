47e907545fc7c09cc50b9dfb368ce07f07c9bf11
rohit <168824659+rohitindurke@users.noreply.github.com>
2024-07-27 19:34:05 +0530

    Add files via upload
    res

--- src/components/form/Otp.jsx
+++ src/components/form/Otp.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import './styles.css';
+import './otp.css';
 import Header from '../Header';
+
 
 function Otp() {
   return (
@@ -11,8 +12,21 @@ function Otp() {
         <div id="box">
         <form>
             <h3 style={{textAlign:'left'}}>
-            currently working on otp and backend
+              Verify With OTP
+             
             </h3>
+              <div className="otp">
+            <input type='text'className='ip' inputmode="numeric" maxlength="1" />
+            <input type='text'className='ip' inputmode="numeric" maxlength="1" />
+            <input type='text'className='ip' inputmode="numeric" maxlength="1" />
+            <input type='text'className='ip' inputmode="numeric" maxlength="1" />
+           </div>
+           <div className="helpText">
+             <b style={{color:'#FE3E6C',textDecoration:'none',fontSize:"13px"}} href="#"> Resend</b><br/>
+             <a style={{color:'#B2B3BA',fontSize:"13px"}}> Login using  </a> <b style={{color:'#FE3E6C',textDecoration:'none',fontSize:"13px"}} href="#"> Password</b><br/>
+
+            <a style={{color:'#B2B3BA',fontSize:"13px"}}> Have trouble logging in?</a> <b style={{color:'#FE3E6C',textDecoration:'none',fontSize:"13px"}} href="#"> Get help</b>
+            </div> 
             </form>
         </div>
         </div>
